8dbb81f4996de637de5b40b414d722e938204202
LENOVO <aarthymaha2000@gmail.com>
2019-02-14 19:33:39 +0530

    initial

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'ebms');
+define('DB_PASSWORD', 'maha*98');
+define('DB_NAME', 'ebms');
+ 
+/* Attempt to connect to MySQL database */
+try{
+    $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
+    // Set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} 
+catch(PDOException $e){
+    die("ERROR: Could not connect. " . $e->getMessage());
+}
+?>
+
